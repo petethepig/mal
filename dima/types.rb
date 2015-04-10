2eeb00c8c2d5624d76da3351e0120c5c7df696b7
Dmitrii Filimonov <me@dfilimonov.com>
2015-04-10 00:29:11 -0700

    step 5

--- dima/types.rb
+++ dima/types.rb
@@ -1,7 +1,10 @@
 class Mal
   attr_reader :type
   attr_reader :val
-  def initialize(type, val)
-    @type, @val = type, val
+  attr_reader :ast
+  attr_reader :params
+  attr_reader :env
+  def initialize(type, val, ast=nil,params=nil,env=nil)
+    @type, @val, @ast, @params, @env = type, val, ast, params, env
   end
 end
