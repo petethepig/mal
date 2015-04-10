a96ccce98377c41bf15636abf195a4f551bc2c1a
Dmitrii Filimonov <me@dfilimonov.com>
2015-04-10 01:07:42 -0700

    `printer` fix for lists

--- dima/printer.rb
+++ dima/printer.rb
@@ -12,11 +12,12 @@ def pr_str(mal, print_readably = false)
     mal.val.to_s
   when :string
     if print_readably
+      # '"' + mal.val.gsub("\"", '\"').gsub("\n", '\n') + '"'
       mal.val.inspect
     else
       mal.val
     end
   when :list
-    "(" + mal.val.map { |x| pr_str(x) }.join(" ") + ")"
+    "(" + mal.val.map { |x| pr_str(x, print_readably) }.join(" ") + ")"
   end
 end
